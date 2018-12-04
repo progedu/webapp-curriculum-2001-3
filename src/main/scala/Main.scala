cae3f729b81471465dc3cba6c8b219c227aaf9ad
Masahiro <masahiro.infinite77@gmail.com>
2018-12-04 15:13:57 +0900

    twiceメソッドの実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,15 @@
 object Main {
 
-  def twice(f: Int => Int): Int => Int = ???
+  //def twice(f: Int => Int): Int => Int = new Function1[Int, Int] {
+  //  def apply(x: Int): Int = f(f(x))
+  //}
+
+  //def twice(f: Int => Int): Int => Int = {
+  //  n: Int => f(f(n))
+  //}  
+
+  def twice(f: Int => Int): Int => Int = (x: Int) => f(f(x))
+
+  //val twice = (f: Int => Int) => (n: Int) => f(f(n))
 
 }
