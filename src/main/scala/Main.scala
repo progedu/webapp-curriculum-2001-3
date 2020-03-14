43ae561df67d3c97b2ea6698e4ef29223044cc16
QWYNG <ikusawasi@gmail.com>
2020-03-14 16:23:57 +0900

    impl  Main.twice

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,9 @@
 object Main {
 
-  def twice(f: Int => Int): Int => Int = ???
+  def twice(f: Int => Int): Int => Int = {
+    new Function1[Int, Int] {
+      def apply(x: Int): Int = f(x)
+    }.andThen[Int](f)
+  }
 
 }
